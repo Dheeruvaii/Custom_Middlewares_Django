92e4a40e84d666ab9881be86abb83b5c2efbe870
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-29 14:04:57 +0545

    create middleware class and initialize it

--- Middleware/soet/middleware.py
+++ Middleware/soet/middleware.py
@@ -1,3 +1,27 @@
-class StackOverFlowMiddleware(object):
-    def process_excption(self,request,exceptions):
+
+# #  trying to write a custom django middleware package
+
+from django.conf import settings
+
+# class StackOverflowMiddleware(object):
+#     def process_exception(self, request, exception):
+#         if settings.DEBUG:
+#             print (exception.__class__.__name__)
+#             print (exception.message)
+#         return None
+    
+
+
+class StackOverflowMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception):
+        if settings.DEBUG:
+            print (exception.__class__.__name__)
+            print (exception.message)
         return None
